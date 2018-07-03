8dae7b44e984c33c26386401db0bee04710255ca
Arthur Epprecht <arthur.epp@gmail.com>
2018-07-03 08:47:37 -0300

    accept packages installation

--- dotnetcore.sh
+++ dotnetcore.sh
@@ -6,6 +6,6 @@ sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 
-sudo apt-get install apt-transport-https
+sudo apt-get install -y apt-transport-https
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1
+sudo apt-get install -y dotnet-sdk-2.1
