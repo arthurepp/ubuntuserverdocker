4f1cad6cfced7f352f5dcf87f5dfe3378a1e137a
Arthur Epprecht <arthur.epp@gmail.com>
2019-03-25 08:11:52 -0300

    Create portainer.sh

---
+++ portainer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo docker volume create portainer_data
+sudo docker run -d -p 9000:9000 --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
