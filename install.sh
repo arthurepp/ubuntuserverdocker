950c3e35d4b3abebeffb0a5b480a5cee735182cf
Arthur Epprecht <arthur.epp@gmail.com>
2020-11-19 23:40:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-compose
-sudo usermod -aG docker "arthur"
+sudo usermod -aG docker $(whoami)
 sudo systemctl enable docker
 sudo reboot
